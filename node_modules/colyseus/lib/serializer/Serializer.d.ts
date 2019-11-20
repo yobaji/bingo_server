2a1654b11f4c9877fcb69ef93c1cc6aafd7aaa5a
Ananthutechversant <ananthutechversant@gmail.com>
2019-11-20 23:05:01 +0530

    Remove node_modules from repository

--- node_modules/colyseus/lib/serializer/Serializer.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from '..';
-export declare function serialize(serializer: new () => Serializer<any>): (constructor: Function) => void;
-export interface Serializer<T> {
-    id: string;
-    reset(data: any): void;
-    getFullState(client: Client): any;
-    applyPatches(clients: Client[], state: T): boolean;
-    handshake?(): number[];
-}
--- node_modules/colyseus/lib/serializer/Serializer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function serialize(serializer) {
-    return (constructor) => {
-        constructor.prototype._getSerializer = () => new serializer();
-    };
-}
-exports.serialize = serialize;
