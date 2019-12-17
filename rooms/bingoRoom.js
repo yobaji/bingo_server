3d4d41a7028934a4b9f193e952f0c97e3922f235
Ananthutechversant <ananthutechversant@gmail.com>
2019-12-17 10:26:26 +0530

    Increace rejoin time out

--- rooms/bingoRoom.js
+++ rooms/bingoRoom.js
@@ -52,7 +52,8 @@ exports.room = class extends colyseus.Room {
     async onLeave(client, consented) {
         if(!this.state.players[client.sessionId])return;
         this.state.playerOffline(client.sessionId, this);
-        let rejoinTimeout = this.locked ? 20 : 0;
+        // let rejoinTimeout = this.locked ? 20 : 0;
+        let rejoinTimeout = 35;
         try {
             if (consented) {
                 throw new Error("consented leave");
