29f68acb647f9f3f6aafd41549daaa7470f6dcd7
Ananthutechversant <ananthutechversant@gmail.com>
2019-12-26 15:43:58 +0530

    Allow cors for all

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.json());
 if(process.env.NODE_ENV == 'development'){
   app.use(cors());
 }else{
-  app.use(cors({origin: 'http://www.vubingo.com'}));
+  // app.use(cors({origin: 'http://www.vubingo.com'}));
+  app.use(cors());
 }
 
 roomController.clearRoomAliases();
