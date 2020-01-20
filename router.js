a087636f7bd6ec3d665a1069685169429165590e
Ananthutechversant <ananthutechversant@gmail.com>
2020-01-20 11:10:15 +0530

    Baji url broken fix

--- router.js
+++ router.js
@@ -12,6 +12,10 @@ exports.createRoutes = function(app){
 
     app.use('/baji_beta', express.static('baji'));
 
+    app.use('/baji', function(req, res) {
+      res.redirect('https://www.facebook.com/Baji321');
+    });
+
     // google web master server ownership proof
     app.get('/googleeedf3d735bfba695.html', function(req, res) {
       res.sendFile(path.join(__dirname + '/assets/googleeedf3d735bfba695.html'));
