c4cbddb174a2ee50d538bfe2099c2434bb910161
Ananthutechversant <ananthutechversant@gmail.com>
2020-01-20 22:00:44 +0530

    Custom fonts added

--- baji/js/particles.js
+++ baji/js/particles.js
@@ -2,7 +2,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 200,
+      "value": 250,
       "density": {
         "enable": true,
         "value_area": 800
@@ -47,7 +47,7 @@ particlesJS("particles-js", {
       }
     },
     "line_linked": {
-      "enable": true,
+      "enable": false,
       "distance": 150,
       "color": "#000",
       "opacity": 0.4,
